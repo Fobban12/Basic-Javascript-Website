da5b9933f8bd52ec59636d2c36dbf23f93e95b49
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-04 15:49:17 +0300

    Added some styling and the start of the look for the page

--- res/scripts.js
+++ res/scripts.js
@@ -1,5 +1,12 @@
-function frontPage(){$('#Content').html("<div>Hello this is a test for jQuery</div>");}
-function sideBar(){$('#Sidebar').html("<div>Hello this is a test for jQuery</div>");}
-function header(){$('#Header').html("<div>Hello this is a test for jQuery</div>");}
-function footer(){$('#Header').html("<div>Hello this is a test for jQuery</div>");}
+
+
+
+
+
+
+function frontPage(){$('#Content').html("<div>Here should be the content for the main page stuff</div>");}
+
+
+
+
 
