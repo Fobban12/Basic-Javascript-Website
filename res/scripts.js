e4c02d464d7195c8aa586a8da4d770ab9fead496
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-04 11:28:55 +0300

    Heroku deployment possible now (app not ready)

--- res/scripts.js
+++ res/scripts.js
@@ -1,2 +1,4 @@
 function frontPage(){$('#Content').html("<div>Hello this is a test for jQuery</div>");}
+function sideBar(){$('#Sidebar').html("<div>Hello this is a test for jQuery</div>");}
+function header(){$('#Header').html("<div>Hello this is a test for jQuery</div>");}
 
