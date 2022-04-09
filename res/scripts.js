acfed098274fee08a7f575b2cc347d50ec19abc6
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-09 18:10:13 +0300

    Sidebar now shows up with press

--- res/scripts.js
+++ res/scripts.js
@@ -3,7 +3,7 @@
 
 function frontPage(){$('#Content').html(`
 <div id="frontPageCSS">
- <div id="deals">Here are the deals, and the pictures of the deals, not sure what yet</div>
+ <div id="deals"> Here are the deals, and the pictures of the deals, not sure what yet</div>
  <div id="products">Here are the products that are recommended for you</div>
  <div id="products">Here are the products that you have seen or some random products that are in the shop</div>
 </div>
@@ -11,7 +11,17 @@ function frontPage(){$('#Content').html(`
 
 );}
 
-function sideBar(){console.log("hello")}
+function sideBar(){$('#SideBar').html(`
+<div id="sideBarCSS">
+ <div>Hello</div>
+ <div>Hello</div>
+ <div>Hello</div>
+</div>
+
+
+
+`),$('#SideBar').toggleClass("show")}
+
 
 
 
