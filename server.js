75187a3fb0526aad231afc5e2425bd177fbd7090
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-11 10:54:27 +0300

    Nah didn't work, looking at this later

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const open = require('open');
 open('http://localhost:3000/')
 
 
+
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -13,32 +16,12 @@ const port = process.env.PORT ||3000;
 
 app.use(express.static(__dirname + '/res'))
 
-
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve(__dirname,"./res/index.html"));
+});
 
 app.all('*', (req, res) => {res.status(404).send("Resource not found")})
 
 
 app.listen(port,() => console.log("App on port: " + port + "!" + "\nGo to localhost:" + port +" on your browser"))
 
-app.get('/db', async (req, res) => {
-    const { Pool } = require('pg');
-    res.sendFile(path.resolve(__dirname,"./res/index.html"))
-    const pool = (() => {
-    return new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-    rejectUnauthorized: false
-    }
-    });
-    })();
-    try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT user_id, username FROM accounts;');
-    const results = { 'results': (result) ? result.rows : null};
-    res.json( results );
-    client.release();
-    } catch (err) {
-    console.error(err);
-    res.json({ error: err });
-    }
-    })
