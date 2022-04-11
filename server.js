ab855e3e17852eb187c467e07ac3783dc7e761d8
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-11 17:05:10 +0300

    Cleaned the code a bit

--- server.js
+++ server.js
@@ -14,17 +14,7 @@ const port = process.env.PORT ||4000;
 app.get('/db', async (req, res) => {
     const { Pool } = require('pg');
     const pool = (() => {
-        //Heroku database
-       /*return new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-        rejectUnauthorized: false
-        }
-        });
-        })();
-        */
-      //Local database
-      if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
         return new Pool({
         connectionString: process.env.DATABASE_URL,
         ssl: false
@@ -40,14 +30,16 @@ app.get('/db', async (req, res) => {
         }
     });
 } })();
-    try {
+
+try {
         const client = await pool.connect();
-        const result = await client.query('SELECT user_id, username FROM accountsTest;');
+        const result = await client.query('SELECT user_id, username FROM accounts;');
         const results = { 'results': (result) ? result.rows : null};
         res.json( results );
         client.release();
     }    
-    catch (err) {
+catch (err) 
+    {
         console.error(err);
          res.json({ error: err });
     }
