fd56a4a48740c5da9ed2868dbc8869898e4a94ee
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-11 11:28:12 +0300

    Cleaned some stuff

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ open('http://localhost:3000/')
 
 
 
-
-
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -14,11 +12,14 @@ const app = express();
 
 const port = process.env.PORT ||3000;
 
+
+
 app.use(express.static(__dirname + '/res'))
 
-app.get('/',(req, res) => {
+//Not needed atm
+/*app.get('/',(req, res) => {
     res.sendFile(path.resolve(__dirname,"./res/index.html"));
-});
+});*/
 
 app.all('*', (req, res) => {res.status(404).send("Resource not found")})
 
