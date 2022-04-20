b73448276a018d79d370be9c15c861b143367917
Jere Muikku <Muikku.jere@hotmail.com>
2022-04-20 15:25:56 +0300

    Added testing for images and start for the deals stuff

--- res/scripts.js
+++ res/scripts.js
@@ -1,40 +1,39 @@
 
 //Loop should be here, remove this later when done
-// The structure of the HTML will be different, on a later date
-function frontPage(){$('#Content').html(`
+//The structure of the HTML will be different, on a later date
+//Pictures should come from the database
+function frontPage(){
+
+
+
+
+
+
+
+
+
+
+
+
+   $('#Content').html(`
 <div id="frontPageCSS">
- <div id="deals"> Here are the deals, and the pictures of the deals, not sure what yet</div>
- <div id="products">Here are the products that are recommended for you</div>
+ <div id="Deals">
+ 
+ <img src="./images/amdDeals.jpg" id="DealPictures"></img>
+ <img src="./images/amdDeals.jpg" id="DealPictures"></img>
+ <img src="./images/amdDeals.jpg" id="DealPictures"></img>
+ <img src="./images/amdDeals.jpg" id="DealPictures"></img>
+ 
+ </div>
+
+ <div id="FrontPageTitle">Recommended for you</div>
+ <div id="products">Recommended Products</div>
+
+ <div id="FrontPageTitle">Popular</div>
  <div id="products">Here are the products that you have seen or some random products that are in the shop</div>
 </div>
 `
-
 );}
-//Loop will be done a bit later, this is just for testing.
-// The structure of the HTML will be different, on a later date
-function sideBar(){
-$('#SideBar').html(`
- <div id="sideBarCSS">
-  <div id="sideBarNames"> <span onclick="productCategoryView()">${category[0].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
-  <div id="sideBarNames"> <span onclick="productCategoryView()">${category[1].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
-  <div id="sideBarNames"> <span onclick="productCategoryView()">${category[2].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
- </div>
- `),$('#SideBar').toggleClass("show")
-};
-
-//This should show the right subcategories based from the category unique id, will be implemented later, will loop trough all the subcategories
-//!!!!!!Right now the css styling is so that it will have an absolute position that will be different based on display size,!!!!fix this later!!!!
-// The structure of the HTML will be different, on a later date
-function subCategory()
-{
-   $('#subCategory').html(`
-   <div>
-    <div id="sideBarNames" onclick="productCategoryView()">${category[1].Name}</div>
-    <div id="sideBarNames" onclick="productCategoryView()">${category[0].Name}</div>
-    <div id="sideBarNames" onclick="productCategoryView()">${category[2].Name}</div>
-   </div>
-   `),$('#subCategory').toggleClass("show")
-  };
 
 
 // Data that comes should be based on the pressed categorys unique ID, so in other words, it will show the items that the category ID is assigned to.
@@ -76,6 +75,32 @@ function productDetailView()
    );
 }
 
+//Loop will be done a bit later, this is just for testing.
+// The structure of the HTML will be different, on a later date
+function sideBar(){
+   $('#SideBar').html(`
+    <div id="sideBarCSS">
+     <div id="sideBarNames"> <span onclick="productCategoryView()">${category[0].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
+     <div id="sideBarNames"> <span onclick="productCategoryView()">${category[1].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
+     <div id="sideBarNames"> <span onclick="productCategoryView()">${category[2].Name}</span> <button id="categoryArrow" onclick="subCategory()"> > </button></div>
+    </div>
+    `),$('#SideBar').toggleClass("show")
+   };
+   
+   //This should show the right subcategories based from the category unique id, will be implemented later, will loop trough all the subcategories
+   //!!!!!!Right now the css styling is so that it will have an absolute position that will be different based on display size,!!!!fix this later!!!!
+   // The structure of the HTML will be different, on a later date
+   function subCategory()
+   {
+      $('#subCategory').html(`
+      <div>
+       <div id="sideBarNames" onclick="productCategoryView()">${category[1].Name}</div>
+       <div id="sideBarNames" onclick="productCategoryView()">${category[0].Name}</div>
+       <div id="sideBarNames" onclick="productCategoryView()">${category[2].Name}</div>
+      </div>
+      `),$('#subCategory').toggleClass("show")
+     };
+   
 
 // The structure of the HTML will be different, on a later date
 function cart()
